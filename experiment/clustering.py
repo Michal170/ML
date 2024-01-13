c0145964515b3538c1c94a0ee14db308d41959f3
Michal170 <93790482+Michal170@users.noreply.github.com>
2024-01-13 12:36:19 +0100

    clusters_visualisation'

--- experiment/clustering.py
+++ experiment/clustering.py
@@ -1,23 +1,10 @@
 import numpy as np
-from sklearn.model_selection import RepeatedStratifiedKFold
-from sklearn.metrics import accuracy_score, balanced_accuracy_score
-from sklearn.base import clone
-import pandas as pd
-from imblearn.metrics import classification_report_imbalanced
-from tabulate import tabulate
+from sklearn.cluster import DBSCAN
+import matplotlib.pyplot as plt
+from sklearn.cluster import OPTICS
 import os
+import pandas as pd
 
-
-DATASETS_name = [
-    "glass.csv",
-    "diabetes.csv",
-    "haberman.csv",
-    "vehicle1.csv",
-    "yeast4.csv",
-    "yeast6.csv",
-    "poker-8-9_vs_5.csv",
-    "poker-8_vs_6.csv",
-]
 
 DATASETS = [
     "datasets/glass.csv",
@@ -29,65 +16,75 @@ DATASETS = [
     "datasets/poker-8-9_vs_5.csv",
     "datasets/poker-8_vs_6.csv",
 ]
-all_class_distributions = pd.DataFrame(columns=["Dataset", "Class", "Sample Count"])
-for ds_idx, dataset_filename in enumerate(DATASETS):
-    dataset = pd.read_csv(
-        dataset_filename,
-        sep=";",
-        skiprows=1,
-    )
-
-    class_distribution = dataset.iloc[:, -1].value_counts().reset_index()
-    class_distribution.columns = ["Class", "Sample Count"]
-
-    class_distribution["Dataset"] = f" {DATASETS_name[ds_idx] }"
-
-    all_class_distributions = pd.concat(
-        [all_class_distributions, class_distribution], ignore_index=True
-    )
-
-# print("Rozkład klas we wszystkich zbiorach danych:")
-# print(tabulate(all_class_distributions, headers="keys", tablefmt="grid"))
-# os.makedirs("results", exist_ok=True)
-# output_file_path = os.path.join("results", f"class_imbalance_ratios.txt")
-# with open(output_file_path, "w") as file:
-#     file.write(tabulate(all_class_distributions, headers="keys", tablefmt="grid"))
+DATASETS_name = [
+    "glass.csv",
+    "diabetes.csv",
+    "haberman.csv",
+    "vehicle1.csv",
+    "yeast4.csv",
+    "yeast6.csv",
+    "poker-8-9_vs_5.csv",
+    "poker-8_vs_6.csv",
+]
 
 
-class_ratios = (
-    all_class_distributions.groupby("Dataset")["Sample Count"].max()
-    / all_class_distributions.groupby("Dataset")["Sample Count"].min()
-)
+def paint_cluster(name, filename):
+    num_datasets = len(DATASETS)
+    num_rows = (num_datasets + 1) // 2
+    num_cols = 2
 
-imbalance_table = pd.DataFrame(
-    {
-        "Dataset": class_ratios.index,
-        "Imbalance Ratio": class_ratios.values,
-    }
-)
+    fig, axs = plt.subplots(num_rows, num_cols, figsize=(12, 8))
 
-
-print("\nStosunek niezbalansowania w zbiorach danych:")
-print(
-    tabulate(
-        imbalance_table,
-        headers="keys",
-        tablefmt="grid",
-        colalign=("right", "right", "right"),
-    )
-)
-
-
-os.makedirs("results", exist_ok=True)
-output_file_path = os.path.join("results", f"imbalance_ratios.txt")
-with open(output_file_path, "w") as file:
-    file.write(tabulate(all_class_distributions, headers="keys", tablefmt="grid"))
-    file.write("\n\n\n")
-    file.write(
-        tabulate(
-            imbalance_table,
-            headers="keys",
-            tablefmt="grid",
-            colalign=("center", "center", "center"),
+    for ds_idx, dataset_filename in enumerate(DATASETS):
+        dataset = pd.read_csv(
+            dataset_filename,
+            sep=";",
+            skiprows=1,
         )
-    )
+        X = dataset.iloc[:, [0, 1]].values
+        y = dataset.iloc[:, -1].values
+
+        methods = name(eps=0.5, min_samples=2)
+        clusters = methods.fit_predict(X)
+        noise_points = clusters == -1
+        count = np.bincount(y[noise_points])
+        unique_labels = set(clusters)
+        colors = [
+            plt.cm.Spectral(each) for each in np.linspace(0, 1, len(unique_labels))
+        ]
+
+        row_idx = ds_idx // num_cols
+        col_idx = ds_idx % num_cols
+
+        for k, col in zip(unique_labels, colors):
+            if k == -1:
+                col = [0.5, 0.5, 0.5, 1]
+                marker = "x"
+            else:
+                marker = "o"
+            class_member_mask = clusters == k
+
+            xy = X[class_member_mask]
+            axs[row_idx, col_idx].plot(
+                xy[:, 0],
+                xy[:, 1],
+                marker,
+                markerfacecolor=tuple(col),
+                markeredgecolor="k",
+                markersize=6,
+            )
+
+        axs[row_idx, col_idx].set_title(f"{DATASETS_name[ds_idx]}")
+
+    if num_datasets % 2 != 0:
+        fig.delaxes(axs[-1, -1])
+
+    plt.tight_layout()
+    results_directory = "results"
+    os.makedirs(results_directory, exist_ok=True)
+    plt.savefig(os.path.join(results_directory, f"{filename}.png"), dpi=300)
+
+
+if __name__ == "__main__":
+    paint_cluster(DBSCAN, "dbscan")
+    paint_cluster(OPTICS, "optics")
